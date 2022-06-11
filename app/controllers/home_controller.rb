d57af70ebf2422e8143653b26a09e3c9473268d5
hekimosath <hekimosath@gmail.com>
2022-06-11 18:49:16 +0900

    質問詳細見た目作成＆トップへのリンク途中
    うまくいってないこと：
    なぜかTOPでquestion_dataのidが全て２になってしまう（おそらくuser_idを参照してる）

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def top
     @hoge = "fugaaaaa"
+    #質問詳細画面
+    @question_data = Question.joins(:user).select("questions.*, users.*")
+    p @question_data
+    unless @question_data
+      render_404
+    end
     pp @hoge
   end
   def article
